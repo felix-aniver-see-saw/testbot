c184012e56f0e8f5da3668193f024941d7b00d51
shua <86475694+felix-aniver-see-saw@users.noreply.github.com>
2022-03-10 02:16:14 +0000

    added main files

---
+++ learning.js
@@ -0,0 +1,32 @@
+//cant believe i forgot everything i learnt about js
+
+//variable declaration
+var name = "value" //function scope
+let name2 = "value2" //block scope
+const name3 = "value3" //immutable
+
+//datatypes
+let string = "69420"
+let integer = 69420
+let boolean = true
+let array = [string, integer, boolean]
+let object = { //not ordered or iterable
+  name_in_python: dictionary
+  retrieve_object: "Object.key(dict)"
+}
+let map = new Map() //ordered and iterable
+map.set("how to get key", "map.get(key)")
+function funct(parameter) {
+  return parameter
+}
+
+//print
+console.log("this is being printed har har")
+
+//control flow
+if(a === a){
+  //== ignores datatypes, === does not
+  //&& is and
+  //|| is or
+  //!= is unequal
+}
